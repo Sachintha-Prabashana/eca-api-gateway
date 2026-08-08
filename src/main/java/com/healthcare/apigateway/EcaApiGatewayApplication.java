39bfe1965346afdb3915d633b2360e6303635804
Sachintha Prabashana <sachinthaprabhashana2003@gmail.com>
2026-08-08 23:12:32 +0530

    feat: Initialize API Gateway with Spring Cloud Gateway and Config Client

---
+++ src/main/java/com/healthcare/apigateway/EcaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcare/apigateway/EcaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
